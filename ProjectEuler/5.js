3838a96d1281b8126c5749c01343d1a420698f19
Vidit Shah <vidit0210@gmail.com>
2019-03-31 18:05:17 +0530

    TimeConversion

--- ProjectEuler/5.js
+++ ProjectEuler/5.js
@@ -1,6 +1,17 @@
 //Even Distribution
-function checDiviible(n){
-    for(let i=0;i<n;i++){
-        
+let s ="07:05:45PM"
+let q=s.split(":")
+console.log()
+function conversion(str){
+    if(str.includes("PM")){
+    let split = str.split(":");
+    let added=parseInt(split[0])+12
+    let am =split.slice(split.length-1)
+   
+    am=am.join('').replace(/\D/g,'')
+    split.splice(0,1,added.toString())
+    return [...split[0],...[':'],...split[1],...[':'],...am].join('')
     }
+    
 }
+console.log(conversion("07:05:45PM"))
